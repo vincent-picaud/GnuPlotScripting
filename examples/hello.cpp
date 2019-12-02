96be0177f055049ad8d154e03b36ff3803c126ed
Picaud Vincent <picaud.vincent@gmail.com>
2019-12-02 22:35:38 +0100

    fix meson files

--- examples/hello.cpp
+++ examples/hello.cpp
@@ -1,9 +1,11 @@
 #include "GnuPlotScripting/hello.hpp"
 #include <iostream>
+#include "fmt/format.h"
 
 using namespace GnuPlotScripting;
 
 int main()
 {
+  std::string s = fmt::format("I'd rather be {1} than {0}.", "right", "happy");
   std::cout << hello() << " from examples/ " << std::endl;
 }
