25be4ecb04ad688bfa2aa944b017d83c09f64396
Picaud Vincent <picaud.vincent@gmail.com>
2019-12-04 16:20:09 +0100

    chg name + cleaning

--- examples/hello.cpp
+++ examples/hello.cpp
@@ -1,3 +1,4 @@
+
 #include "GnuPlotScripting/data_vector.hpp"
 #include "GnuPlotScripting/script.hpp"
 
@@ -12,12 +13,14 @@ main()
 {
   std::vector<double> v1(10, 5);
   std::valarray<int> v2(10);
-  std::string v3("01234567891");
+  std::string v3("0123456789");
   Data_Vector data(v1, v2, v3);
 
   Script_File script("script.gp");
 
-  script.free_form("plot sin(x) t \"super\"");
-  script.free_form("replot {} w l t \"ca marche?\"", data);
-  script.free_form("replot {} u ($1)+2 w l lw 3 t \"oui!?\"", data);
+  script.fmt_free_form("plot sin(x) t \"super\"");
+  script.fmt_free_form("replot {} w l t \"ca marche?\"", data);
+  script.fmt_free_form("replot {} u ($1)+2 w l lw 3 t \"oui!?\"", data);
+  script.export_as(Export_As_PNG().set_color(false), "test.png");
+  script.export_as(Export_As_PNG().set_color(false), fmt::format("{}.png", "test2"));
 }
