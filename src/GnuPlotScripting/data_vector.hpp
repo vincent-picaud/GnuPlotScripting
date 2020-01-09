374473379ca4d24d61ba2f059ae1333b58799840
Picaud Vincent <picaud.vincent@gmail.com>
2020-01-09 21:21:02 +0100

    cleaning data_vector

--- src/GnuPlotScripting/data_vector.hpp
+++ src/GnuPlotScripting/data_vector.hpp
@@ -25,32 +25,54 @@ namespace GnuPlotScripting
   class Data_Vector final : public Data
   {
     template <typename VECTOR, typename... VECTORs>
-    std::string
-    create_embedded_data(const VECTOR& vector, const VECTORs&... vectors)
-    {
-      static_assert(has_size_method_v<VECTOR> && (has_size_method_v<VECTORs> && ...));
-      static_assert(has_random_access_operator_v<VECTOR> &&
-                    (has_random_access_operator_v<VECTORs> && ...));
+    static std::string create_embedded_data(const VECTOR& vector, const VECTORs&... vectors);
 
-      const size_t n = vector.size();
-      assert(((n == vectors.size()) && ...));
-
-      std::stringstream buffer;
-      for (size_t i = 0; i < n; ++i)
-      {
-        buffer << vector[i] << " ";
-        ((buffer << vectors[i] << " "), ...);
-        buffer << "\n";
-      }
-
-      return buffer.str();
-    }
+    std::size_t _I_size;
+    std::size_t _J_size;
 
    public:
     template <typename VECTOR, typename... VECTORs>
     Data_Vector(const VECTOR& vector, const VECTORs&... vectors)
-        : Data(create_embedded_data(vector, vectors...))
+        : Data(create_embedded_data(vector, vectors...)),
+          _I_size(vector.size()),
+          _J_size(1 + sizeof...(VECTORs))
     {
     }
+
+    std::size_t
+    I_size() const noexcept
+    {
+      return _I_size;
+    };
+    std::size_t
+    J_size() const noexcept
+    {
+      return _J_size;
+    };
   };
+
+  //////////////////////////////////////////////////////////////////
+
+  template <typename VECTOR, typename... VECTORs>
+  std::string
+  Data_Vector::create_embedded_data(const VECTOR& vector, const VECTORs&... vectors)
+  {
+    static_assert(has_size_method_v<VECTOR> && (has_size_method_v<VECTORs> && ...));
+    static_assert(has_random_access_operator_v<VECTOR> &&
+                  (has_random_access_operator_v<VECTORs> && ...));
+
+    const size_t n = vector.size();
+    assert(((n == vectors.size()) && ...));
+
+    std::stringstream buffer;
+    for (size_t i = 0; i < n; ++i)
+    {
+      buffer << vector[i] << " ";
+      ((buffer << vectors[i] << " "), ...);
+      buffer << "\n";
+    }
+
+    return buffer.str();
+  }
+
 }
