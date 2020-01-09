b23ad41beff4a277c4c418783576bb6ff4078759
Picaud Vincent <picaud.vincent@gmail.com>
2020-01-09 22:22:59 +0100

    more data_vector test

--- test/data_vector.cpp
+++ test/data_vector.cpp
@@ -17,11 +17,13 @@ TEST(Data_Std_Vector, uuid)
 
 TEST(Data_Std_Vector, vector)
 {
-  std::vector<double> v1(2, 3);
-  std::vector<int> v2(2, 4);
+  std::vector<double> v1(4, 3);
+  std::vector<int> v2(4, 4);
 
   Data_Vector test_1(v1, v2);
-  ASSERT_EQ(test_1.data(), "3 4 \n3 4 \n");
+  ASSERT_EQ(test_1.data(), "3 4 \n3 4 \n3 4 \n3 4 \n");
+  ASSERT_EQ(test_1.I_size(), 4);
+  ASSERT_EQ(test_1.J_size(), 2);
 }
 
 TEST(Data_Std_Vector, copy)
@@ -33,7 +35,9 @@ TEST(Data_Std_Vector, copy)
   Data_Vector test_2(v2, v1);
   ASSERT_EQ(test_1.data(), "3 4 \n3 4 \n");
 
-  test_2 = test_1;
+  test_2 = test_1;  // check that copy is supported
+
   ASSERT_EQ(test_1.data(), "3 4 \n3 4 \n");
   ASSERT_EQ(test_2.data(), "3 4 \n3 4 \n");
+  ASSERT_EQ(test_1.uuid(), test_2.uuid());
 }
